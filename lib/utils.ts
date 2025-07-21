1a5e693cddb05d572fd4dabc0fd65e14e7e858d5
Pipin16 <pantorik@gmail.com>
2025-07-21 17:39:16 +0700

    fixurl

--- lib/utils.ts
+++ lib/utils.ts
@@ -43,7 +43,7 @@ export function getUrl() {
   if (process.env.NODE_ENV === "development") {
     return process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
   } else {
-    return process.env.NEXT_PUBLIC_WEB_URL || "https://kisah-sukses-pmi.vercel.app";
+    return process.env.NEXT_PUBLIC_WEB_URL || "https://kisahsuksescorta.pmi.or.id";
   }
 }
 
